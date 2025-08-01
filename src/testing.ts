9cc25459fac59b131aaafb38ad059b224c9d522d
prooheckcp <61127277+prooheckcp@users.noreply.github.com>
2025-08-01 09:58:45 +0100

    Setting compiler

--- src/testing.ts
+++ src/testing.ts
@@ -1,5 +1,10 @@
-import {Folder, File} from "./FileManager"
+import {Folder, File} from "./FileManager.js"
 import path from "path"
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // cannot resolve path
 
